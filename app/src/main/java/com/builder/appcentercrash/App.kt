8abf1e5527b08d565ec09089449b02338aaf73d8
Shabaz Ahmed <shabaz1729@gmail.com>
2021-04-02 17:23:24 +0530

    added video

--- app/src/main/java/com/builder/appcentercrash/App.kt
+++ app/src/main/java/com/builder/appcentercrash/App.kt
@@ -10,9 +10,6 @@ import com.microsoft.appcenter.distribute.Distribute
 class App: Application() {
     override fun onCreate() {
         super.onCreate()
-        AppCenter.setLogLevel(Log.VERBOSE)
-        AppCenter.start(
-            this, "e4ab3755-5c79-43bc-9eba-a02ee5071145", Distribute::class.java
-        )
+
     }
 }
